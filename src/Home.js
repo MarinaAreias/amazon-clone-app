47c9e099caea7e98019befecd97029792745fb4e
MarinaAreias <marinasareias@gmail.com>
2021-01-03 22:13:38 +0100

    Home.js
    -added Header component to the checkout div
    -created Home component
    -added img
    -styled img on Home.css

---
+++ src/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Home.css";
+
+export default function Home() {
+  return (
+    <div className="home">
+      <img
+        className="home__img"
+        src="https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Events/2021/NYNY/Fuji_TallHero_NYNY_en_US_2x._CB412256578_.jpg"
+        alt=""
+      />
+    </div>
+  );
+}
