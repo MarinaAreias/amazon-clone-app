5aaac0b18016615137fbdb971c81cbfdf5bcc278
MarinaAreias <marinasareias@gmail.com>
2021-01-04 23:39:06 +0100

    Login functionality
    - create function for buttons to work
    -used firebase website to enable Authentication
    -created hooks with useStates
    - used auth from firebase

--- src/Login.js
+++ src/Login.js
@@ -1,8 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Login.css";
 import { Link, useHistory } from "react-router-dom";
+import { auth } from "./firebase";
 
 function Login() {
+  //see React docs on useState hook
+  //with this we can go to input and use the values
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const login = (event) => {
+    event.preventDefault();
+    //login logic here
+    //this function is buind into firebase
+
+    auth
+      .signInWithEmailAndPassword(email, password)
+      .then((auth) => {
+        //logged in, redirect to home page.
+      })
+      .catch((event) => alert(event.message));
+  };
+
+  const register = (event) => {
+    event.preventDefault();
+    //register logic here
+    auth
+      .createUserWithEmailAndPassword(email, password)
+      .then((auth) => {
+        //created user and logged in
+      })
+      .catch((event) => alert(event.message));
+  };
+
   return (
     <div className="login">
       <Link to="/">
@@ -17,10 +47,22 @@ function Login() {
         <h1>sign in</h1>
         <form>
           <h5>E-mail </h5>
-          <input type="email" />
+          <input
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+            type="email"
+          />
           <h5>Password</h5>
-          <input type="password" />
-          <button type="submit" className="login__sign-in-button">
+          <input
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+            type="password"
+          />
+          <button
+            onClick={login}
+            type="submit"
+            className="login__sign-in-button"
+          >
             Sign In
           </button>
         </form>
@@ -29,7 +71,7 @@ function Login() {
           By signing in, you agree to Amazon's Conditions of Use and Privacy
           Notice.
         </p>
-        <button className="login__register-button">
+        <button onClick={register} className="login__register-button">
           {" "}
           Create your Amazon Account
         </button>
